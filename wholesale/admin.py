3084439d271dc3efce64cab0ae740ea53b91aa03
sw.stanleywu@gmail.com <sw.stanleywu@gmail.com>
2019-05-11 12:31:56 -0700

    combined databases

--- wholesale/admin.py
+++ wholesale/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Discount, Category, Products, Prod_dis, Prod_order, Customers, Payment, ShippingAddress, ShippingMethod, Order
 
-# Register your models here.
+admin.site.register(Discount)
+admin.site.register(Category)
+admin.site.register(Products)
+admin.site.register(Prod_dis)
+admin.site.register(Prod_order)
+admin.site.register(Customers)
+admin.site.register(Payment)
+admin.site.register(ShippingAddress)
+admin.site.register(ShippingMethod)
+admin.site.register(Order)
